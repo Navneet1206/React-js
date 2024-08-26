a86529b20c771c4da8556daa3138573885f16082
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-08-26 15:53:06 +0530

    Component for the percentage maker

---
+++ React-use-state/src/components/Cll.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Cll.css"
+
+const percentagebar = () => {
+  return (
+    <div className='content'> <p>80 %</p>
+    <div className='outer'>
+        <div className="inner">
+        </div>
+    </div>
+  </div>
+  )
+}
+
+export default percentagebar
